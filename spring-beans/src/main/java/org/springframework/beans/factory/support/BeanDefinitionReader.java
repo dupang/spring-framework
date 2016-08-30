0e673b47403f471f96c6f566bc9ac787d6123402
dupang <372850928@qq.com>
2016-08-30 17:32:31 +0800

    AbstractBeanFactory

--- spring-beans/src/main/java/org/springframework/beans/factory/support/BeanDefinitionReader.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/BeanDefinitionReader.java
@@ -24,13 +24,20 @@ import org.springframework.core.io.ResourceLoader;
  * Simple interface for bean definition readers.
  * Specifies load methods with Resource and String location parameters.
  *
+ * bean定义读取器的简单接口。
+ * 指定带着资源和String的路径参数的加载方法。
+ *
  * <p>Concrete bean definition readers can of course add additional
  * load and register methods for bean definitions, specific to
  * their bean definition format.
  *
+ * 具体的bean定义读取器可以为bean定义增加额外的加载和注册方法，针对他们的bean定义格式。
+ *
  * <p>Note that a bean definition reader does not have to implement
  * this interface. It only serves as suggestion for bean definition
  * readers that want to follow standard naming conventions.
+ *
+ * 注意一个bean定义读取器不必要实现这个接口。它只是作为想保留标准的命名约定的bean定义的读取器的建议。
  *
  * @author Juergen Hoeller
  * @since 1.1
@@ -42,6 +49,8 @@ public interface BeanDefinitionReader {
 	 * Return the bean factory to register the bean definitions with.
 	 * <p>The factory is exposed through the BeanDefinitionRegistry interface,
 	 * encapsulating the methods that are relevant for bean definition handling.
+	 * 返回bean工厂用来注册bean定义的注册器。
+	 * 工厂被暴露通过BeanDefinitionRegistry接口，封闭了bean定义处理相关的方法。
 	 */
 	BeanDefinitionRegistry getRegistry();
 
@@ -59,6 +68,15 @@ public interface BeanDefinitionReader {
 	 * <p>There is also a {@code loadBeanDefinitions(String)} method available,
 	 * for loading bean definitions from a resource location (or location pattern).
 	 * This is a convenience to avoid explicit ResourceLoader handling.
+	 * 返回资源路径使用的资源加载器。可以被检查ResourcePatternResolver接口和相应地类型转换，
+	 * 对于给定的资源模式加载多个资源。
+	 * null表示绝对的资源加载是不可用的对于bean定义读取器。
+	 * 这主要是想被用来引入进一步的资源从一个bean定义的资源，例如在XML的bean定义中通过"import"标签。
+	 * 然而建议应用这样的import想对于定义的资源;只有显式的完全资源路径将触发绝对的资源加载。
+	 * 这也有一个可用的loadBeanDefinitions(String)方法，用于人一个资源路径(或路径模式)来加载bean定义。
+	 * 这是一个便利避免显式的资源加载器处理。
+	 *
+	 *
 	 * @see #loadBeanDefinitions(String)
 	 * @see org.springframework.core.io.support.ResourcePatternResolver
 	 */
@@ -69,18 +87,22 @@ public interface BeanDefinitionReader {
 	 * <p>{@code null} suggests to not load bean classes eagerly
 	 * but rather to just register bean definitions with class names,
 	 * with the corresponding Classes to be resolved later (or never).
+	 * 返回bean类的类加载器。
+	 * null说明不急切地加载bean类而只是用类名注册bean定义，相应的类将被以后解析。
 	 */
 	ClassLoader getBeanClassLoader();
 
 	/**
 	 * Return the BeanNameGenerator to use for anonymous beans
 	 * (without explicit bean name specified).
+	 * 返回匿名bean使用的BeanNameGenerator(没有显式地指定bean名称)
 	 */
 	BeanNameGenerator getBeanNameGenerator();
 
 
 	/**
 	 * Load bean definitions from the specified resource.
+	 * 从指定的资源中加载bean定义。
 	 * @param resource the resource descriptor
 	 * @return the number of bean definitions found
 	 * @throws BeanDefinitionStoreException in case of loading or parsing errors
@@ -89,6 +111,7 @@ public interface BeanDefinitionReader {
 
 	/**
 	 * Load bean definitions from the specified resources.
+	 * 从指定的资源中加载bean定义。
 	 * @param resources the resource descriptors
 	 * @return the number of bean definitions found
 	 * @throws BeanDefinitionStoreException in case of loading or parsing errors
@@ -99,6 +122,9 @@ public interface BeanDefinitionReader {
 	 * Load bean definitions from the specified resource location.
 	 * <p>The location can also be a location pattern, provided that the
 	 * ResourceLoader of this bean definition reader is a ResourcePatternResolver.
+	 * 从指定的资源路径中加载bean定义。
+	 * 位置可以是一个位置模式，假如bean定义读取器的ResourceLoader是一个ResourcePatternResolver。
+	 *
 	 * @param location the resource location, to be loaded with the ResourceLoader
 	 * (or ResourcePatternResolver) of this bean definition reader
 	 * @return the number of bean definitions found
@@ -111,6 +137,7 @@ public interface BeanDefinitionReader {
 
 	/**
 	 * Load bean definitions from the specified resource locations.
+	 * 从指定的资源路径中加载bean定义。
 	 * @param locations the resource locations, to be loaded with the ResourceLoader
 	 * (or ResourcePatternResolver) of this bean definition reader
 	 * @return the number of bean definitions found
