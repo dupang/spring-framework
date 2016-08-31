aac154652f350fb3bf82f81c11f4929f42234c90
dupang <372850928@qq.com>
2016-08-31 11:47:19 +0800

    AbstractBeanFactory

--- spring-beans/src/main/java/org/springframework/beans/factory/xml/BeanDefinitionDocumentReader.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/xml/BeanDefinitionDocumentReader.java
@@ -24,10 +24,15 @@ import org.springframework.beans.factory.BeanDefinitionStoreException;
  * SPI for parsing an XML document that contains Spring bean definitions.
  * Used by {@link XmlBeanDefinitionReader} for actually parsing a DOM document.
  *
+ * 用于解析包含Spring的bean定义的XML文档的SPI.
+ *
  * <p>Instantiated per document to parse: implementations can hold
  * state in instance variables during the execution of the
  * {@code registerBeanDefinitions} method &mdash; for example, global
  * settings that are defined for all bean definitions in the document.
+ *
+ * 每一个解析的文档初实例化:实现可以持有在实例变量中的状态在registerBeanDefinitions方法的过程中;
+ * 例如，用于文档中的所有bean定义的全局设置。
  *
  * @author Juergen Hoeller
  * @author Rob Harrop
@@ -39,6 +44,8 @@ public interface BeanDefinitionDocumentReader {
 	/**
 	 * Read bean definitions from the given DOM document and
 	 * register them with the registry in the given reader context.
+	 *
+	 * 从给定的DOM文档中读取bean定义并且注册到给定的读上下文中的注册器中。
 	 * @param doc the DOM document
 	 * @param readerContext the current context of the reader
 	 * (includes the target registry and the resource being parsed)
