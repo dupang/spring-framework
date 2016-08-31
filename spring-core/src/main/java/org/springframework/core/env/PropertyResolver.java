72b425cb31b17e8099b60cd12b1da43e528753ce
dupang <372850928@qq.com>
2016-08-31 14:59:38 +0800

    AbstractBeanFactory

--- spring-core/src/main/java/org/springframework/core/env/PropertyResolver.java
+++ spring-core/src/main/java/org/springframework/core/env/PropertyResolver.java
@@ -19,6 +19,8 @@ package org.springframework.core.env;
 /**
  * Interface for resolving properties against any underlying source.
  *
+ * 用于解析任何底层资源的属性。
+ *
  * @author Chris Beams
  * @author Juergen Hoeller
  * @since 3.1
@@ -30,12 +32,15 @@ public interface PropertyResolver {
 	/**
 	 * Return whether the given property key is available for resolution,
 	 * i.e. if the value for the given key is not {@code null}.
+	 *
+	 * 返回是否给定的属性key对于解析是否可用，也就是说。是否给定的key的值不为null。
 	 */
 	boolean containsProperty(String key);
 
 	/**
 	 * Return the property value associated with the given key,
 	 * or {@code null} if the key cannot be resolved.
+	 * 返回和给定的key关联的属性值，或者null如果key不能被解析。
 	 * @param key the property name to resolve
 	 * @see #getProperty(String, String)
 	 * @see #getProperty(String, Class)
@@ -46,6 +51,9 @@ public interface PropertyResolver {
 	/**
 	 * Return the property value associated with the given key, or
 	 * {@code defaultValue} if the key cannot be resolved.
+	 *
+	 * 返回和给定的key关联的属性值，或者默认仩如果key不能被解析。
+	 *
 	 * @param key the property name to resolve
 	 * @param defaultValue the default value to return if no value is found
 	 * @see #getRequiredProperty(String)
@@ -56,6 +64,7 @@ public interface PropertyResolver {
 	/**
 	 * Return the property value associated with the given key,
 	 * or {@code null} if the key cannot be resolved.
+	 * 返回和给定的key关联的属性值，或者null如果key不能被解析。
 	 * @param key the property name to resolve
 	 * @param targetType the expected type of the property value
 	 * @see #getRequiredProperty(String, Class)
@@ -65,6 +74,7 @@ public interface PropertyResolver {
 	/**
 	 * Return the property value associated with the given key,
 	 * or {@code defaultValue} if the key cannot be resolved.
+	 * 返回和给定的key关联的属性值，或者默认值如果key不能被解析。
 	 * @param key the property name to resolve
 	 * @param targetType the expected type of the property value
 	 * @param defaultValue the default value to return if no value is found
@@ -76,6 +86,8 @@ public interface PropertyResolver {
 	 * Return the property value associated with the given key (never {@code null}).
 	 * @throws IllegalStateException if the key cannot be resolved
 	 * @see #getRequiredProperty(String, Class)
+	 *
+	 * 返回和给定的key关联的属性值(永远不为null)。如果key不能被解析就返回IllegalStateException。
 	 */
 	String getRequiredProperty(String key) throws IllegalStateException;
 
@@ -83,6 +95,9 @@ public interface PropertyResolver {
 	 * Return the property value associated with the given key, converted to the given
 	 * targetType (never {@code null}).
 	 * @throws IllegalStateException if the given key cannot be resolved
+	 *
+	 * 返回和给定的key关联的属性值，转换为给定的目标类型(永远不为null)。
+	 * 如果key不能被解析就返回IllegalStateException。
 	 */
 	<T> T getRequiredProperty(String key, Class<T> targetType) throws IllegalStateException;
 
@@ -90,6 +105,10 @@ public interface PropertyResolver {
 	 * Resolve ${...} placeholders in the given text, replacing them with corresponding
 	 * property values as resolved by {@link #getProperty}. Unresolvable placeholders with
 	 * no default value are ignored and passed through unchanged.
+	 *
+	 * 解析给定的文本中的${...}占位符，用相应的被getProperty解析的属性值替换他们。没有默认值的不能解析的占位符
+	 * 被忽略并且原封不动地传递下去。
+	 *
 	 * @param text the String to resolve
 	 * @return the resolved String (never {@code null})
 	 * @throws IllegalArgumentException if given text is {@code null}
@@ -102,6 +121,11 @@ public interface PropertyResolver {
 	 * Resolve ${...} placeholders in the given text, replacing them with corresponding
 	 * property values as resolved by {@link #getProperty}. Unresolvable placeholders with
 	 * no default value will cause an IllegalArgumentException to be thrown.
+	 *
+	 *
+	 * 解析给定的文本中的${...}占位符，用相应的被getProperty解析的属性值替换他们。没有默认值的不能解析的占位符
+	 * 将导致抛出IllegalArgumentException。
+	 *
 	 * @return the resolved String (never {@code null})
 	 * @throws IllegalArgumentException if given text is {@code null}
 	 * or if any placeholders are unresolvable
