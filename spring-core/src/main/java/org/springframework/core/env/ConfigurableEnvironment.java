1c4b36839fa873455aee607b01a7f13aad3b222d
dupang <372850928@qq.com>
2016-09-18 17:07:24 +0800

    DefaultBeanNameGenerator

--- spring-core/src/main/java/org/springframework/core/env/ConfigurableEnvironment.java
+++ spring-core/src/main/java/org/springframework/core/env/ConfigurableEnvironment.java
@@ -25,6 +25,9 @@ import java.util.Map;
  * conversion service and more through the {@link ConfigurablePropertyResolver}
  * superinterface.
  *
+ * 将被大部分如果不是所有的Environment类型实现的配置接口。提供工具来设置活跃和默认的profile并且维护底层的
+ * 属性资源。允许客户端来设置和校验需要的属性，自定义转换服务和更多通过ConfigurablePropertyResolver父接口。
+ *
  * <h2>Manipulating property sources</h2>
  * <p>Property sources may be removed, reordered, or replaced; and additional
  * property sources may be added using the {@link MutablePropertySources}
@@ -32,6 +35,13 @@ import java.util.Map;
  * are against the {@link StandardEnvironment} implementation of
  * {@code ConfigurableEnvironment}, but are generally applicable to any implementation,
  * though particular default property sources may differ.
+ *
+ * 维护属性源
+ *
+ * 属性可能被删除，重排序，或替换;并且另外的属性源可以使用被getPropertySources()返回的
+ * MutablePropertySources实例被增加。
+ * 下面的例子对应着ConfigurableEnvironment的StandardEnvironment实现，但是通常也适用于任何实现，
+ * 尽管特定的默认属性源可能不同。
  *
  * <h4>Example: adding a new property source with highest search priority</h4>
  * <pre class="code">
@@ -64,6 +74,9 @@ import java.util.Map;
  * org.springframework.context.support.PropertySourcesPlaceholderConfigurer property
  * placeholder configurers}.
  *
+ * 当一个Environment被ApplicationContext使用，它是重要的任何这样的PropertySource操作被执行
+ * 在context的refresh()方法调用之前。它确保所有的属性资源可用在容器的启动过程中，包括被
+ * PropertySourcesPlaceholderConfigurer属性占位符配置器的使用。
  *
  * @author Chris Beams
  * @since 3.1
@@ -79,6 +92,12 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * <p>Any existing active profiles will be replaced with the given arguments; call
 	 * with zero arguments to clear the current set of active profiles. Use
 	 * {@link #addActiveProfile} to add a profile while preserving the existing set.
+	 *
+	 * 指定这个Environment启用的profile集合。Profiles被求值在容器启动过程中来判断是否bean定义
+	 * 是否应该被注册到这个容器上。
+	 * 任何存在的启用的profiles将被替换为给定的参数;带着0个参数来调用将清除当前启用的profile.
+	 * 使用addActiveProfile来增加一个profile当保存存在的集合时。
+	 *
 	 * @see #addActiveProfile
 	 * @see #setDefaultProfiles
 	 * @see org.springframework.context.annotation.Profile
@@ -91,6 +110,8 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * Add a profile to the current set of active profiles.
 	 * @see #setActiveProfiles
 	 * @throws IllegalArgumentException if the profile is null, empty or whitespace-only
+	 *
+	 * 在当前的活跃的profiles中增加一个profile。
 	 */
 	void addActiveProfile(String profile);
 
@@ -99,6 +120,8 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * are explicitly made active through {@link #setActiveProfiles}.
 	 * @see AbstractEnvironment#DEFAULT_PROFILES_PROPERTY_NAME
 	 * @throws IllegalArgumentException if any profile is null, empty or whitespace-only
+	 *
+	 * 指定默认启用的profile集合如果没有其它profiles被显式地通过setActiveProfiles启用。
 	 */
 	void setDefaultProfiles(String... profiles);
 
@@ -116,6 +139,13 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * sources such as the set of system properties or the set of system environment
 	 * variables.
 	 * @see AbstractEnvironment#customizePropertySources
+	 *
+	 * 以可变的形式返回这个Environment的PropertySources，允许操作当根据这个Environment
+	 * 对象解析属性时应该被搜索的PropertySource对象的集合
+	 *
+	 * 不同的MutablePropertySources方法例如：
+	 * ...
+	 * 允许的对属性源顺序进行详细的控制。这是很有用的，例如，为了确保一些用户定义的属性源比默认的属性源有优先搜索顺序。
 	 */
 	MutablePropertySources getPropertySources();
 
@@ -131,6 +161,14 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * {@link IllegalAccessException}; in cases where the SecurityManager forbids access
 	 * to a property, {@code null} will be returned and an INFO-level log message will be
 	 * issued noting the exception.
+	 *
+	 * 返回System#getenv()的值，如果被当前的SecurityManager允许。否则返回一个map实现，这个map将试图
+	 * 访问单独的keys使用System#getenv(String)调用。
+	 * 注意大部分Environment实现将包括这个系统环境map作为一个将被搜索的默认的PropertySource。因此，
+	 * 建议这个方法不被直接使用除非绕过其它属性源是明确的目的。
+	 *
+	 * 在返回的Map上调用Map#get(Object)将永远不会抛出IllegalAccessException;在SecurityManager
+	 * 禁止访问一个Property的情况下，null将被返回并且一个INFO-level的日志消息将被抛出而不是异常。
 	 */
 	Map<String, Object> getSystemEnvironment();
 
@@ -146,6 +184,8 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * {@link IllegalAccessException}; in cases where the SecurityManager forbids access
 	 * to a property, {@code null} will be returned and an INFO-level log message will be
 	 * issued noting the exception.
+	 *
+	 *
 	 */
 	Map<String, Object> getSystemProperties();
 
@@ -163,6 +203,16 @@ public interface ConfigurableEnvironment extends Environment, ConfigurableProper
 	 * the parent environment occurring after the call to {@code merge} will not be
 	 * reflected in the child. Therefore, care should be taken to configure parent
 	 * property sources and profile information prior to calling {@code merge}.
+	 *
+	 * 附加给定的父环境的启用的profile,默认的profiles和property源到这个环境的相应的集合中。
+	 * 对任何存在于父和孩子中的同名的PropertySource实例，孩子实例将被保留并且父实例丢弃。这有允许被孩子
+	 * 的属性源的覆盖的作用也有避免重复的搜索通过通过属性源类型，例如系统环境和系统属性。
+	 *
+	 * 启用的和默认的profile名称也被过滤为了重复，为了避免混淆和过多的存储。
+	 *
+	 * 父环境仍然不可修改在任何情况下。注意在调用merge之后对父环境的任何改变将不会反映到孩子中。
+	 * 因此应该小心来配置父属性源和profile信息在调用merge之前。
+	 *
 	 * @param parent the environment to merge with
 	 * @since 3.1.2
 	 * @see org.springframework.context.support.AbstractApplicationContext#setParent
