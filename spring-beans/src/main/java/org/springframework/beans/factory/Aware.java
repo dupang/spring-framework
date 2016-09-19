8c4efb37a87aec60d2cbfb57ab7342ce8e021cd8
dupang <372850928@qq.com>
2016-09-19 17:44:43 +0800

    Aware

--- spring-beans/src/main/java/org/springframework/beans/factory/Aware.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/Aware.java
@@ -24,12 +24,18 @@ package org.springframework.beans.factory;
  * consist of just one void-returning method that accepts a single
  * argument.
  *
+ * 标记超级接口表示一个bean是有资格被Spring的特定框架对象容器通知通过一个回调类型的方法通知。
+ * 真正的方法标记被单独的子接口确定，但是通常应该只有一个空返回值的方法组成。
+ *
  * <p>Note that merely implementing {@link Aware} provides no default
  * functionality. Rather, processing must be done explicitly, for example
  * in a {@link org.springframework.beans.factory.config.BeanPostProcessor BeanPostProcessor}.
  * Refer to {@link org.springframework.context.support.ApplicationContextAwareProcessor}
  * and {@link org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory}
  * for examples of processing {@code *Aware} interface callbacks.
+ *
+ * 注意公公实现Awarer不会提供默认的功能。相反，过程必须显式地被完成，例如，在一个BeanPostProcessor。
+ * 参考ApplicationContextAwareProcessor和AbstractAutowireCapableBeanFactory关于处理接口回调的例子。
  *
  * @author Chris Beams
  * @since 3.1
