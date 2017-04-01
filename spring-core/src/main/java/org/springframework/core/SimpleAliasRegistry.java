be730ceb7998b1172a9acc63bcb7f2ed53d77f89
duguanxin <duguanxin@damai.cn>
2017-04-01 12:00:40 +0800

    update

--- spring-core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/SimpleAliasRegistry.java
@@ -197,6 +197,7 @@ public class SimpleAliasRegistry implements AliasRegistry {
 
 	/**
 	 * Determine the raw name, resolving aliases to canonical names.
+	 * 确定原始名称，解析别名为规范名
 	 * @param name the user-specified name
 	 * @return the transformed name
 	 */
