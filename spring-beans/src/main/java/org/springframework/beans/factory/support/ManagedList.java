7766e1cb74a30345e319b709742236d9044d9fd9
duguanxin <duguanxin@damai.cn>
2017-04-06 11:56:48 +0800

    update

--- spring-beans/src/main/java/org/springframework/beans/factory/support/ManagedList.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/ManagedList.java
@@ -16,15 +16,17 @@
 
 package org.springframework.beans.factory.support;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.BeanMetadataElement;
 import org.springframework.beans.Mergeable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Tag collection class used to hold managed List elements, which may
  * include runtime bean references (to be resolved into bean objects).
+ *
+ * 标签集合被用来持有管理的列表元素，可能包含运行时bean引用(被解析为bean对象).
  *
  * @author Rod Johnson
  * @author Rob Harrop
@@ -52,6 +54,9 @@ public class ManagedList<E> extends ArrayList<E> implements Mergeable, BeanMetad
 	/**
 	 * Set the configuration source {@code Object} for this metadata element.
 	 * <p>The exact type of the object will depend on the configuration mechanism used.
+	 *
+	 * 设置为这个元数据元素的配置源对象。
+	 * 对象的具体类型将依赖使用的配置机制。
 	 */
 	public void setSource(Object source) {
 		this.source = source;
@@ -64,6 +69,8 @@ public class ManagedList<E> extends ArrayList<E> implements Mergeable, BeanMetad
 
 	/**
 	 * Set the default element type name (class name) to be used for this list.
+	 *
+	 * 设置这个列表的被使用的默认元素类型名称。
 	 */
 	public void setElementTypeName(String elementTypeName) {
 		this.elementTypeName = elementTypeName;
@@ -71,6 +78,8 @@ public class ManagedList<E> extends ArrayList<E> implements Mergeable, BeanMetad
 
 	/**
 	 * Return the default element type name (class name) to be used for this list.
+	 *
+	 * 返回这个列表的被使用的默认元素类型名称。
 	 */
 	public String getElementTypeName() {
 		return this.elementTypeName;
@@ -79,6 +88,8 @@ public class ManagedList<E> extends ArrayList<E> implements Mergeable, BeanMetad
 	/**
 	 * Set whether merging should be enabled for this collection,
 	 * in case of a 'parent' collection value being present.
+	 *
+	 * 设置是否合并应该被启用为这个集合，在'父'集合出现的时候。
 	 */
 	public void setMergeEnabled(boolean mergeEnabled) {
 		this.mergeEnabled = mergeEnabled;
